31fb6515014471dd8c706477899a1276c91515e3
Predrag Mandic <predrag@phlogisto.com>
2014-12-17 02:31:29 +0100

    rearanged project to be able to use pip on github

--- markdownship/file.py
+++ markdownship/file.py
@@ -1,6 +1,7 @@
 """Collection of file operating wrapper functions.
 """
 
+import os
 
 def read(file_name):
   """Read a file on path file_name and return a string with it's contents."""
@@ -19,6 +20,19 @@ def write(file_name, data):
       myfile.write(data)
   except Exception, err:
     raise Exception("Error writting file: " + file_name)
+
+
+def find_mkd(root_path):
+  """Searches for files with .md and .mkd extensions and returns a list of
+  relative paths to "root_path". """
+  mkd_file_list = []
+  for root, dirs, files in os.walk(root_path):
+    for file in files:
+      if file.lower().endswith((".md", ".mkd")):
+        relative_file_path = os.path.join(root, file)[len(root_path):]
+        mkd_file_list.append(relative_file_path)
+  return mkd_file_list
+  
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from distutils.core import setup
 
 setup(
   name='markdownship',
-  version='0.1.0',
+  version="0.1.0",
   description='Markdown to html converter.',
   author='Predrag Mandic',
   author_email='predrag@phlogisto.com',
   packages=['markdownship', ],
+  install_requires = [
+    'argparse==1.2.1',
+    'markdown==2.3.1',
+    ],
   )
 
