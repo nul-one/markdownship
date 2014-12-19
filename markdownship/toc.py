f0438610db7644b74e6103e3abfd53d1fe8ec928
Predrag Mandic <predrag@phlogisto.com>
2014-12-19 13:03:46 +0100

    sort toc entries alphabetically and replace _'s with spaces

--- markdownship/toc.py
+++ markdownship/toc.py
@@ -12,13 +12,13 @@ def create(root_path, template=None, mkd_tag=None, debug=False):
   """Creates tables of contents for each directory as index.html file"""
   for path, dirs, files in os.walk(root_path):
     index_mkd = ""
-    for d in dirs:
+    for d in sorted(dirs):
       link = os.path.join(os.path.curdir, os.path.join(d,"index.html"))
-      index_mkd += "["+d.upper()+"]("+link+")  \n"
-    for f in files:
+      index_mkd += "* [[ "+d.replace('_',' ')+" ]]("+link+")\n\n"
+    for f in sorted(files):
       if f.lower().endswith(".html") and f != "index.html":
         link = os.path.join(os.path.curdir, os.path.splitext(f)[0]+".html")
-        index_mkd += "["+os.path.splitext(f)[0]+"]("+link+")  \n"
+        index_mkd += "* ["+os.path.splitext(f)[0]+"]("+link+")\n\n"
     index_html = mkd_to_html(
       index_mkd,
       template = template,
